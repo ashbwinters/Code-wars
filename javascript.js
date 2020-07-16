1474f65edfdb5ba605ec44e5c3ad1197d925deeb
Ashley Winters <ashbwinters@gmail.com>
2020-07-16 09:14:13 -0600

    find the outlier completed with Ben Dunn and Sam Evans. 7.16.20

--- javascript.js
+++ javascript.js
@@ -1,21 +1,14 @@
-// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
-
-// Examples
-// [2, 4, 0, 100, 4, 11, 2602, 36]
-// Should return: 11 (the only odd number)
-
-// [160, 3, 1719, 19, 11, 13, -21]
-// Should return: 160 (the only even number)
+// Find the Outlier KYU 6
 
 function findOutlier(integers){
-    function isOdd(num) {
-      return (num % 2) == 1
-    }
-    function isEven(num) {
-      return (num % 2) == 0
-    }
-    const odd = integers.filter(num => isOdd(num))
-    const even = integers.filter(num => isEven(num))
+    const odds = []
+    const evens = []
     
-    return odd.length == 1 ? odd[0] : even[0]
+    function sortInts(num) {
+      return (num % 2) == 0 ? evens.push(num) : odds.push(num)
+    }
+    
+    integers.map(num => sortInts(num))
+    
+    return odds.length == 1 ? odds[0] : evens[0]
   }
