1823e0076b2fe4266ed01f7db9d7624a28cbeb4b
Ashley Winters <ashbwinters@gmail.com>
2020-07-22 08:47:57 -0600

    List filtering 7.22.20

--- javascript.js
+++ javascript.js
@@ -1,20 +1,26 @@
 // Find the Outlier KYU 6
 
 function findOutlier(integers){
-    const odds = []
-    const evens = []
+  const odds = []
+  const evens = []
     
-    function sortInts(num) {
-      return (num % 2) == 0 ? evens.push(num) : odds.push(num)
-    }
-    
-    integers.map(num => sortInts(num))
-    
-    return odds.length == 1 ? odds[0] : evens[0]
+  function sortInts(num) {
+    return (num % 2) == 0 ? evens.push(num) : odds.push(num)
   }
+    
+  integers.map(num => sortInts(num))
+    
+  return odds.length == 1 ? odds[0] : evens[0]
+}
 
 //   Dubstep KYU 6
 
-function songDecoder(song){
-    return song.replace(/WUB/g, " ").replace(/\s+/g, " ").trim()
-  }
+function songDecoder(song) {
+  return song.replace(/WUB/g, " ").replace(/\s+/g, " ").trim()
+}
+
+// List Filtering
+
+function filter_list(l) {
+  return l.filter( element => Number.isInteger(element))
+}
